09c571bfb2885d9a3992e872ae2863c324289436
Karanveer Singh <166089567+Karanveer100@users.noreply.github.com>
2025-06-30 17:28:06 +0530

    Add files via upload

---
+++ 0nfform.sql
@@ -0,0 +1,23 @@
+CREATE TABLE UNF_COURSE_SELECTION (
+  Student_ID VARCHAR2(10),
+  Student_Name VARCHAR2(40),
+  Student_Dept VARCHAR2(20),
+  Student_Contact VARCHAR2(15),
+  Student_Email VARCHAR2(40),
+  CGPA NUMBER(3,2),
+  Advisor_ID VARCHAR2(10),
+  Advisor_Name VARCHAR2(40),
+  Advisor_Email VARCHAR2(40),
+  Advisor_Contact VARCHAR2(15),
+  Course_ID VARCHAR2(10),
+  Course_Name VARCHAR2(40),
+  Course_Credit VARCHAR2(10),
+  Teacher_ID VARCHAR2(10),
+  Teacher_Name VARCHAR2(40),
+  Teacher_Email VARCHAR2(40),
+  Teacher_Address VARCHAR2(50),
+  Fee_ID VARCHAR2(10),
+  Fee_Amount VARCHAR2(20),
+  Feedback_ID VARCHAR2(10),
+  Feedback_Report VARCHAR2(40)
+);
